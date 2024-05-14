9d69fd13a567e2de5e1fd586aeaa273c0db2307d
wookookie <wookookie404@gmail.com>
2024-05-14 16:40:14 +0900

    Create auth service logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,12 +2,9 @@
  * Controller: Auth
  */
 
-import crypto from "node:crypto";
 import { Request, Response, NextFunction } from "express";
 import passport from "passport";
-import config from "../config/config";
-import dataSource from "../datasource";
-import { User } from "../entity/user.entity";
+import service from "../services/auth.service";
 
 function signin(req: Request, res: Response, next: NextFunction) {
   // FIX-ME: any type
@@ -33,41 +30,14 @@ function signin(req: Request, res: Response, next: NextFunction) {
   })(req, res, next);
 }
 
-async function signup(req: Request, res: Response, next: NextFunction) {
+async function signup(req: Request, res: Response) {
   const { email, password } = req.body;
+  const createdUser = await service.createUser(email, password);
 
-  // falsy 값 확인
-  if (!email || !password) {
-    console.log("[Auth] Invalid email or password");
-    return res.status(400).json({ code: "INVALID_REQUEST", message: "Invalid email or password" });
-  }
-
-  try {
-    const userRepo = dataSource.getRepository(User);
-
-    const foundUser = await userRepo.findOne({ where: { email } });
-    if (foundUser) {
-      return res.status(400).json({ code: "ALREADY_EXIST", message: "Already exist user" });
-    }
-
-    // Password crypto
-    const salt = crypto.randomBytes(64);
-    crypto.pbkdf2(password, salt, config.CRYPTO_ITERATIONS, 64, "sha512", async (error, derivedKey) => {
-      if (error) {
-        throw error;
-      }
-
-      const newUser = new User();
-      newUser.email = email;
-      newUser.password = derivedKey.toString("hex");
-      newUser.salt = salt.toString("hex");
-      userRepo.save(newUser);
-
-      return res.status(201).json({ result: "registered" });
-    });
-  } catch (error) {
-    console.error(error);
-    next(error);
+  if (createdUser.return === "created") {
+    return res.status(201).json(createdUser.result);
+  } else if (createdUser.return === "error") {
+    return res.status(400).json(createdUser.result);
   }
 }
 
